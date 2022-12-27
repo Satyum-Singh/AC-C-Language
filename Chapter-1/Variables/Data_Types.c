5e395073a756cb0435ecffb4d89d62d4b79f7c90
Satyum <satyumspathania@gmail.com>
2022-12-27 13:19:38 +0530

    Done with constants

---
+++ Chapter-1/Variables/Data_Types.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    // == INT == for integer values.
+    int age = 22;
+    float pi = 3.14;
+    char hashtag = '#';
+    return 0;
+}
---
+++ Chapter-1/Variables/Implementation.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int number =25;
+    char star='*';
+    int age = '20';
+    float pi = 3.14;
+    return 0;
+}
